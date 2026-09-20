cac30103b8db9d15aa134ff7663403921285bb85
yuan777coder <a3109702698a@163.com>
2026-09-20 21:43:39 +0800

    Add files via upload

---
+++ libraries/zf_components/stc32g_usb_lib/usb_desc.h
@@ -0,0 +1,13 @@
+#ifndef __USB_DESC_H__
+#define __USB_DESC_H__
+
+extern unsigned char code DEVICEDESC[18];
+extern unsigned char code CONFIGDESC[67];
+extern unsigned char code LANGIDDESC[4];
+extern unsigned char code MANUFACTDESC[8];
+extern unsigned char code PRODUCTDESC[30];
+
+extern unsigned char code PACKET0[2];
+extern unsigned char code PACKET1[2];
+
+#endif
